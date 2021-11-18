2b874101583ac5840ab9189fef605037461ef29c
Jareya <81638685+Jareya@users.noreply.github.com>
2021-11-18 11:35:43 -0500

    Update Calendar Organizer.py

--- Calendar Organizer.py
+++ Calendar Organizer.py
@@ -1,1 +1,6 @@
-# EMU Software Engineering Project
+# EMU Software Engineering Project, Fall 2021
+# Calendar Organizer
+# Contributors: Jareya Harder, Seth Brenneman, Duncan Ferency-Viars, Alijah Ellis
+# version 0
+
+#Code
