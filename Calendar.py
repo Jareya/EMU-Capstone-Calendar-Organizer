95fa419f3d0dd503f3c553694a61841ef79de720
Kyle_Miller <42157646+Kyle-19@users.noreply.github.com>
2022-02-16 15:01:10 -0500

    Update Calendar.py

--- Calendar.py
+++ Calendar.py
@@ -2,13 +2,13 @@ from tkinter import *
 from tkcalendar import Calendar
 
 # Create background
-root = Tk()
+eventCreation = Tk()
 icon = PhotoImage(file="quill.ico")
-root.iconphoto(False, icon)
-root.title("Calendar")
+eventCreation.iconphoto(False, icon)
+eventCreation.title("Calendar")
 
 # Functions to show and hide the date picker
-cal = Calendar(root, selectmode='day', borderwidth=3)
+cal = Calendar(eventCreation, selectmode='day', borderwidth=3)
 cal.grid(column=0, row=1, columnspan=2, rowspan=1)
 
 
@@ -20,60 +20,8 @@ def hide_cal():
     cal.grid_forget()
 
 
-Button(root, text='Show Calendar', padx=20, command=show_cal).grid(column=0, row=0)
-Button(root, text='Hide Calendar', padx=21, command=hide_cal).grid(column=1, row=0)
-
-# Month view
-months = (("January", 31), ("February", 28), ("March", 31), ("April", 30), ("May", 31), ("June", 30),
-          ("July", 31), ("August", 31), ("September", 30), ("October", 31), ("November", 30), ("December", 31))
-monthNum = 0
-monthTuple = months[monthNum]
-monthView = Tk()
-
-
-def monthUp():
-    global monthNum, monthTuple
-    monthNum = (monthNum + 1) % 12
-    monthTuple = months[monthNum]
-    createView()
-    print(monthNum)
-
-
-def monthDown():
-    global monthNum, monthTuple
-    monthNum = (monthNum - 1) % 12
-    monthTuple = months[monthNum]
-    createView()
-    print(monthNum)
-
-
-def createView():
-    monthView.title("Calendar")
-    Button(monthView, text='<', padx=20, command=monthDown).grid(column=0, row=0)
-    Button(monthView, text='>', padx=20, command=monthUp).grid(column=5, row=0)
-    Label(monthView, text=monthTuple[0], padx=20).grid(column=0, row=0, columnspan=6)
-    i = 1
-    column = 0
-    row = 1
-    while i <= monthTuple[1]:
-        if column <= 5:
-            if i < 10:
-                date = Label(monthView, text=i, padx=53, pady=50)
-                date.grid(column=column, row=row)
-                column += 1
-                i += 1
-                continue
-            date = Label(monthView, text=i, padx=53, pady=50)
-            date.grid(column=column, row=row)
-            column += 1
-            i += 1
-            continue
-        row += 1
-        column = 0
-        i += 1
-
-
-createView()
+Button(eventCreation, text='Show Calendar', padx=20, command=show_cal).grid(column=0, row=0)
+Button(eventCreation, text='Hide Calendar', padx=21, command=hide_cal).grid(column=1, row=0)
 
 # Event Creation
 startDate = None  # string
@@ -109,22 +57,77 @@ def create_event():
     eventName = event_name.get()
     description = event_description.get('1.0', 'end-1c')
     eventData = [startDate, endDate, eventName, description]
-    print(eventData)
+    # print(eventData)
+    # TODO - Store the event data in sqlite 3 database
     return eventData
 
 
-Button(root, text='Select Start Date', padx=17, command=start_date).grid(column=0, row=2)
-Button(root, text='Select End Date', padx=18, command=end_date).grid(column=1, row=2)
+Button(eventCreation, text='Select Start Date', padx=17, command=start_date).grid(column=0, row=2)
+Button(eventCreation, text='Select End Date', padx=18, command=end_date).grid(column=1, row=2)
 
-Label(root, text="Event Name").grid(column=0, row=3, columnspan=2)
-event_name = Entry(root, borderwidth=5, width=40)
+Label(eventCreation, text="Event Name").grid(column=0, row=3, columnspan=2)
+event_name = Entry(eventCreation, borderwidth=5, width=40)
 event_name.grid(column=0, row=4, columnspan=2)
 
-Label(root, text="Event Description").grid(column=0, row=5, columnspan=2)
-event_description = Text(root, borderwidth=5, height=20, width=30)
+Label(eventCreation, text="Event Description").grid(column=0, row=5, columnspan=2)
+event_description = Text(eventCreation, borderwidth=5, height=20, width=30)
 event_description.grid(column=0, row=6, columnspan=2)
 
-Button(root, text='Create Event', padx=27, command=create_event).grid(column=0, row=7)
-Button(root, text='Clear', padx=43, command=clear).grid(column=1, row=7)
+Button(eventCreation, text='Create Event', padx=27, command=create_event).grid(column=0, row=7)
+Button(eventCreation, text='Clear', padx=43, command=clear).grid(column=1, row=7)
 
-root.mainloop()
+
+# Code to generate the month view of the calendar
+months = (("January", 31), ("February", 28), ("March", 31), ("April", 30), ("May", 31), ("June", 30),
+          ("July", 31), ("August", 31), ("September", 30), ("October", 31), ("November", 30), ("December", 31))
+monthNum = 0  # tracks what month the month view is on, starting with 0 which is january
+monthTuple = months[monthNum]  # To store the pairing of month and number of days for use in create_view()
+monthView = Tk()
+monthView.title("Calendar")
+gridList = []  # Tracks anything placed onto the month grid, so it can be removed when selecting another month
+
+
+def month_up():  # executed when the > Button is pressed in the app
+    global monthNum, monthTuple
+    clear_grid()
+    monthNum = (monthNum + 1) % 12  # so that the list keeps cycling when you get to december
+    monthTuple = months[monthNum]
+    create_view()
+
+
+def month_down():  # executed when the < Button is pressed in the app
+    global monthNum, monthTuple
+    clear_grid()
+    monthNum = (monthNum - 1) % 12
+    monthTuple = months[monthNum]
+    create_view()
+
+
+def clear_grid():  # Clears out anything created and placed on the grid so that the next month can be placed
+    for i in gridList:
+        i.grid_forget()
+
+
+def create_view():  # fills the month view grid with the number of dates based on the current month
+    Label(monthView, text=monthTuple[0], padx=20).grid(column=1, row=0, columnspan=4)
+    day = 1  # no comment
+    column = 0
+    row = 1
+    while day <= monthTuple[1]:  # Example monthTuple = ("January", 31) | so prevents having too many days in month
+        if column <= 5:  # Keep doing this chunk until it reaches 6 wide
+            date = Label(monthView, text=day, padx=53, pady=50)
+            date.grid(column=column, row=row)
+            gridList.append(date)  # Store the created object in the list, so it can be removed later
+            column += 1
+            day += 1
+            continue
+        row += 1  # Move to next row
+        column = 0  # Reset column counter
+        day += 1
+
+
+Button(monthView, text='<', padx=20, command=month_down).grid(column=0, row=0)
+Button(monthView, text='>', padx=20, command=month_up).grid(column=5, row=0)
+create_view()  # Create the first month
+
+eventCreation.mainloop()
